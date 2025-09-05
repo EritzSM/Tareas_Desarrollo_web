1c1a10227fc73d1a8cb2e14bb0214aa81c31ba0b
Eritz <edasame63@gmail.com>
2025-09-04 21:23:50 -0500

    tarea 3

---
+++ Tarea_3/insta-pic-app/src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { Login } from './features/pages/login/login';
+import { SignUp } from './features/pages/sign-up/sign-up';
+import { Home } from './features/pages/home/home';
+
+export const routes: Routes = [
+    {
+        path:"",
+        component:Login
+    },
+    {
+        path:"sign-up",
+        component:SignUp
+    },
+    {
+        path:"home",
+        component:Home
+    },
+    {
+        path:"**",
+        redirectTo:""
+    }
+];
