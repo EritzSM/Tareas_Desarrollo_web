bb44e0ab5c2860d89044255182632dfba5c2709a
Eritz <edasame63@gmail.com>
2025-09-06 10:41:36 -0500

    Validacion password y repassword

---
+++ Tarea_4/insta-pic-app/src/app/shared/services/auth.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+import { LoginRespose, SignUpResponse } from '../interfaces/login-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth {
+
+  login(user:User):LoginRespose{
+
+        let userStr = localStorage.getItem(user.username);
+        if(userStr && user.password === JSON.parse(userStr).password){
+            return {success:true}; 
+        }
+        return {success:false, message:'Usuario o contraseña incorrectos'};
+
+    }
+
+
+    onSignUp(user:User):SignUpResponse{
+
+        let userStr = localStorage.getItem(user.username!);
+        if(userStr){
+            return {success:false, message:'Ya existe el Usuario'}; 
+        }
+        if(user.password !== user.rePassword)
+            return {success:false, message:'Las contraseñas no coinciden'};
+        localStorage.setItem(user.username!, JSON.stringify(user));
+        return {success:true, redirectTo:'home'};
+
+    }
+  
+}
